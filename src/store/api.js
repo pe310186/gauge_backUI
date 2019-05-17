bb7ebd68589baf577869a5b7a06355bbda7bf96a
pe310186 <pe310186@gmail.com>
2019-05-17 14:47:43 +0800

    new product type

--- src/store/api.js
+++ src/store/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let client = axios.create({
-    //baseURL:   'http://localhost:3000',
+    // baseURL:   'http://localhost:3000',
     baseURL: 'http://210.61.46.101:8787',
     // timeout: 60000
 })
